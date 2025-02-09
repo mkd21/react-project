898ae01e0b0a68a77d3edb5460c4c917033bee9f
Mayank Deep <rishusinha525@gmail.com>
2025-02-10 01:46:33 +0530

    added responsive design code

--- POKEMON-CARDS/pokemon-card/src/App.jsx
+++ POKEMON-CARDS/pokemon-card/src/App.jsx
@@ -1,25 +1,15 @@
 
 import ParentComp from "./COMPONENTS/ParentComp/pokemonDataFetchComp.jsx";
-<<<<<<< HEAD
 
-function App() {
- 
-=======
+
 import "./App.css";
-
-
 function App() {
-  
->>>>>>> 0245bf045e58944e25244ebd965aad9414416a2c
-  return (
-    <>
-      <ParentComp />
-    </>
-  )
-}
+    
+    return (
+      <>
+        <ParentComp />
+      </>
+    )
+  }
 
-<<<<<<< HEAD
-export default App
-=======
-export default App
->>>>>>> 0245bf045e58944e25244ebd965aad9414416a2c
+export default App;
--- POKEMON-CARDS/pokemon-card/src/main.jsx
+++ POKEMON-CARDS/pokemon-card/src/main.jsx
@@ -6,9 +6,6 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-<<<<<<< HEAD
+  </StrictMode>
+
 )
-=======
-)
->>>>>>> 0245bf045e58944e25244ebd965aad9414416a2c
